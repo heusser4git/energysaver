08bf610ec41d63618d34d2b1e9f6312c87a5821c
Urs Heusser <urs.heusser@fhgr.ch>
2022-06-12 08:35:16 +0200

    webroute uh removed

--- control/routes/webRoutes.mjs
+++ control/routes/webRoutes.mjs
@@ -10,10 +10,6 @@ Router.get("/:file", (req, res)=>{
     res.sendFile(path.join(process.cwd() + '/view/' + req.params.file));
 });
 
-Router.get("/uh/:file", (req, res)=>{
-    res.sendFile(path.join(process.cwd() + '/view/uh/' + req.params.file));
-});
-
 Router.get("/img/:file", (req, res)=>{
     res.sendFile(path.join(process.cwd() + '/view/img/' + req.params.file));
 });
